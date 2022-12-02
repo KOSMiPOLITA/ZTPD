3ea252c2e198789037b56889a54a51a09a74685f
Eryk Kosmala <73111059+KOSMiPOLITA@users.noreply.github.com>
2022-12-02 14:19:24 +0100

    lab07

---
+++ Oracle Spatial: Układy LRS/Linear Referencing System.sql
@@ -0,0 +1,74 @@
+--cw1
+
+--A
+
+create table A6_LRS (GEOM SDO_GEOMETRY);
+
+--B
+
+select * from STREETS_AND_RAILROADS;
+
+select geom from MAJOR_CITIES where city_name = 'Koszalin' ;
+
+insert into A6_LRS
+select SR.geom
+from STREETS_AND_RAILROADS SR, MAJOR_CITIES C
+where SDO_WITHIN_DISTANCE(SR.geom, C.geom, 'distance = 10 unit = km') = 'TRUE'
+and C.CITY_NAME = 'Koszalin';
+
+--C
+
+select SDO_GEOM.SDO_LENGTH(SR.GEOM, 1, 'unit=km') DISTANCE, 
+ST_LINESTRING(SR.GEOM) .ST_NUMPOINTS() ST_NUMPOINTS
+from A6_LRS SR;
+
+--D
+
+update A6_LRS set geom = SDO_LRS.CONVERT_TO_LRS_GEOM(GEOM, 0, 276.681);
+
+--E
+
+INSERT INTO USER_SDO_GEOM_METADATA
+VALUES ('A6_LRS','GEOM',
+MDSYS.SDO_DIM_ARRAY(
+MDSYS.SDO_DIM_ELEMENT('X', 12.603676, 26.369824, 1),
+MDSYS.SDO_DIM_ELEMENT('Y', 45.8464, 58.0213, 1),
+MDSYS.SDO_DIM_ELEMENT('M', 0, 300, 1) ),
+8307);
+
+--F
+
+CREATE INDEX a6_lrs_idx ON A6_LRS(GEOM) INDEXTYPE IS MDSYS.SPATIAL_INDEX;
+
+--cw2
+
+--A
+
+select SDO_LRS.VALID_MEASURE(GEOM, 500) VALID_500
+from A6_LRS;
+
+--B
+select SDO_LRS.GEOM_SEGMENT_END_PT(GEOM).Get_WKT() END_PT
+from A6_LRS;
+
+--C
+
+select SDO_LRS.LOCATE_PT(GEOM, 150, 0).Get_WKT() KM150 
+from A6_LRS;
+
+--D
+
+select SDO_LRS.CLIP_GEOM_SEGMENT(GEOM, 120, 160).Get_WKT() CLIPPED 
+from A6_LRS;
+
+--E
+
+select SDO_LRS.GET_NEXT_SHAPE_PT(a.geom, C.GEOM).Get_WKT() WJAZD_NA_A6
+from A6_LRS a, MAJOR_CITIES C where C.CITY_NAME = 'Slupsk';
+
+--F
+
+select SDO_LRS.OFFSET_GEOM_SEGMENT(A6.GEOM, M.DIMINFO, 50, 200, 50,
+'unit=m arc_tolerance=0.05') KOSZT
+from A6_LRS A6, USER_SDO_GEOM_METADATA M
+where M.TABLE_NAME = 'A6_LRS' and M.COLUMN_NAME = 'GEOM';
